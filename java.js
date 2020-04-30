c58076f7352eb7d38945313495526e30cccbe73b
AnilDaal <62748868+AnilDaal@users.noreply.github.com>
2020-04-30 19:13:58 +0530

    new data

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
  console.log('hello dear');
- 
- 
+ let myobj = {
+     name: 56,
+ }
+ let js = '           anil   '
+ console.log(`${js.toUpperCase()} ${js.trim()}`);
+ let ne =  function(vari){
+return vari.name
+   
+ }
+ console.log(ne(myobj));
+let result = 22/7
+console.log(result.toFixed(3));
